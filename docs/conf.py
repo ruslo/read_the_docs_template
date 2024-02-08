7cab883bbdda6a990d1ef9a90c39ee0772cc4fca
Ruslan Baratov <x@ruslo.dev>
2024-02-08 17:28:21 +0800

    Release 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project_copyright = f'2024, {author}'
 github_url = 'https://github.com/ruslo/read_the_docs_template'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 # The short X.Y version
 version = packaging.version.parse(release)
