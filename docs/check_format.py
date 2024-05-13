ef8d6de34b2915375c1dc38d468385063152f2f2
Ruslan Baratov <x@ruslo.dev>
2024-05-14 02:01:02 +0800

    check_format.py: Add '--apply'/'--force'

--- docs/check_format.py
+++ docs/check_format.py
@@ -4,10 +4,11 @@
 
 import difflib
 import os
+import shutil
 import subprocess
 import sys
-import time
 import tempfile
+import time
 
 from argparse import ArgumentParser
 from pathlib import Path
@@ -34,20 +35,47 @@ def skip_dir(relative_dir):
       return True
   return False
 
-def run_main():
-  """Wrapper for main"""
+def parse_args():
+  """Parse arguments"""
+  parser = ArgumentParser(description=__doc__)
+  group = parser.add_mutually_exclusive_group()
+  a_arg = group.add_argument('-a', '--apply', action='store_true',
+      help='apply formatting for a single file')
+  f_arg = group.add_argument('-f', '--force', action='store_true',
+      help='force formatting for all files')
+  arg_apply_hints = ''
+  for arg in [a_arg, f_arg]:
+    arg_apply_hints += f'\n  {arg.option_strings} {arg.help}'
+  args = parser.parse_args()
+  return args.apply, args.force, arg_apply_hints
+
+def check_configuration(script_dir):
+  """Check configuration file"""
+  cfg = script_dir / '.clang-format'
+  print(f'Configuration: {cfg}')
+  assert cfg.exists()
+
+def get_script_dir():
+  """This script directory"""
   this_script = Path(__file__)
   assert this_script.exists()
 
   script_dir = this_script.parent
   assert script_dir.is_dir()
+  return script_dir
 
-  parser = ArgumentParser(description=__doc__)
-  parser.parse_args()
+def run_main():
+  """Wrapper for main"""
+  script_dir = get_script_dir()
+  assert script_dir.is_dir()
 
-  cfg = script_dir / '.clang-format'
-  print(f'Configuration: {cfg}')
-  assert cfg.exists()
+  apply, force, arg_apply_hints = parse_args()
+  assert not apply is None
+  assert not force is None
+  assert not arg_apply_hints is None
+  assert arg_apply_hints != ''
+
+  check_configuration(script_dir)
 
   for root, _, files in os.walk(script_dir):
     relative = Path(root).relative_to(script_dir)
@@ -64,14 +92,23 @@ def run_main():
         continue
       with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as tmp:
         tmp.write(formatted)
-      if original.rstrip('\n') == formatted.rstrip('\n'):
-        print('\nInconsistent newline(s) at end of file')
-      else:
-        diff = difflib.unified_diff(original.splitlines(),
-            formatted.splitlines(), fromfile=str(to_check), tofile=tmp.name)
-        print('\n'.join(diff))
-      print(f'\nApply hint:\n  mv {tmp.name} {to_check}')
-      sys.exit(1)
+      if not apply and not force:
+        if original.rstrip('\n') == formatted.rstrip('\n'):
+          print('\nInconsistent newline(s) at end of file')
+        else:
+          diff = difflib.unified_diff(original.splitlines(),
+              formatted.splitlines(), fromfile=str(to_check), tofile=tmp.name)
+          print('\n'.join(diff))
+        print(f'\nApply hint:\n\n  mv {tmp.name} {to_check}')
+        print(f'\nAlso:\n{arg_apply_hints}')
+        sys.exit(1)
+
+      print(f'Applying formatting: {tmp.name} -> {to_check}')
+      # Pathlib.replace (possible) error: Invalid cross-device link
+      shutil.move(tmp.name, to_check)
+
+      # '--apply' will only be performed once
+      apply = False
 
 if __name__ == '__main__':
   start = time.time()
